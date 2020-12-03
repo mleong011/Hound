2718ab54fe28ca617a3b86f45905196f1b608622
Mary Leong <mleong011@gmail.com>
2020-12-03 16:39:58 -0500

    google login button

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import CardsForm from "./pages/CardsForm";
 import GetPassword from "./pages/GetPassword";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import SigninPage from './pages/SigninPage';
+
+
 
 class App extends React.Component {
 	render() {
@@ -16,7 +19,9 @@ class App extends React.Component {
 					<Route path="/sign-up" component={SignUp} />
 					<Route path="/get-password" component={GetPassword} />
 					<Route path="/search" component={CardsForm} />
+					<Route path="/sign-in" component={SigninPage}/>
 					<Route path="/" component={Login} />
+					
 				</Switch>
 			</Router>
 		);
