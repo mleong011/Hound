29dd96285d7c35d2d732e0a624ff7138709c9435
sateshr <ramnathsatesh100@gmail.com>
2020-12-10 11:54:07 -0500

    removed shipping table not needed anymore

--- api/models/Order.js
+++ api/models/Order.js
@@ -26,14 +26,6 @@ module.exports = (sequelize, DataTypes) => {
       hooks: true
     })
 
-    //1-1: One Order has one set of Shipment Information
-    // Creates orderId on Shipping model and table
-    models.Order.hasOne(models.Shipment, {
-      foreignKey: {name: 'orderId'},
-      onDelete: 'CASCADE',
-      onUpdate: 'CASCADE',
-      hooks: true
-    });
   }
 
   return Order;
